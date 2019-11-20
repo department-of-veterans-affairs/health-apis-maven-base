e23ee925108011857362b631912b2cf3b3ee8e88
Beau Grantham <beau.grantham@va.gov>
2019-11-20 10:11:45 -0500

    Add release steps

--- build.sh
+++ build.sh
@@ -9,15 +9,17 @@ VERSION=${VERSION:-$(cat $BASEDIR/VERSION)}
 do_build() {
   local java_version=$1
   case "$java_version" in
-    8) tags=(3.5-jdk-8 latest) ;;
-    12) tags=(3.6-jdk-12) ;;
+    8) local tags=(3.5-jdk-8 latest) ;;
+    12) local tags=(3.6-jdk-12) ;;
     *) echo "Unknown Java version: $java_version. Supported versions are 8 and 12." && exit 1 ;;
   esac
   docker build -f $BASEDIR/Dockerfile$java_version $(sed "s#\([^ ]\+\)#-t $REPOSITORY:\1-$VERSION#g" <<< ${tags[@]}) $BASEDIR
   if [ $RELEASE == true ]; then
     docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
     for tag in "${tags[@]}"; do
-      echo "docker push $REPOSITORY:$tag"
+      docker tag $REPOSITORY:$tag-$VERSION $REPOSITORY:$tag
+      docker rmi $REPOSITORY:$tag-$VERSION
+      docker push $REPOSITORY:$tag
     done
   fi
 }
