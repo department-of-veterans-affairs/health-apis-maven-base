0dad530ce2af93a6e856b0923f741c8799b6eccb
Bryan Schofield <bryan.schofield@libertyits.com>
2020-01-06 13:44:10 -0500

    Removing all traces of adding specific certificates to the Maven images

--- build.sh
+++ build.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
-#
-# ------ HACK ------
-#
-# The certs have been removed from the maven base, this will break all new resulting builds.
-# To prevent this change from percolating, we are disabling builds of the base at this time.
-# Follow-up fix to come soon...
-exit 0
-
 BASEDIR=$(dirname $0)
 RELEASE=${RELEASE:-false}
 REPOSITORY=vasdvp/health-apis-maven
