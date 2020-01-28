84f842d94c3958ab54a3dbf03abf700585987652
Bryan Schofield <bryan.schofield@libertyits.com>
2020-01-28 08:23:34 -0500

    Removed Maven 3.5/Java 8 (Dockerfile8) support

--- build.sh
+++ build.sh
@@ -5,14 +5,18 @@ RELEASE=${RELEASE:-false}
 REPOSITORY=vasdvp/health-apis-maven
 VERSION=${VERSION:-$(cat $BASEDIR/VERSION)}
 
-do_build() {
-  local java_version=$1
-  case "$java_version" in
-    8) local tags=(3.5-jdk-8 latest) ;;
+#
+# This builds a version of the base image. In the past we supported multiple version, e.g. Java 8 and Java 12.
+# In the future, as we migrate to the next version of Java, we will also need to support multiple version.
+#
+buildMavenImage() {
+  local javaVersion=$1
+  # Tag format is {maven.version}-jdk-{java.major.version}
+  case "$javaVersion" in
     12) local tags=(3.6-jdk-12) ;;
-    *) echo "Unknown Java version: $java_version. Supported versions are 8 and 12." && exit 1 ;;
+    *) echo "Unknown Java version: $javaVersion. Supported versions: 12" && exit 1 ;;
   esac
-  docker build -f $BASEDIR/Dockerfile$java_version $(sed "s#\([^ ]\+\)#-t $REPOSITORY:\1-$VERSION#g" <<< ${tags[@]}) $BASEDIR
+  docker build -f $BASEDIR/Dockerfile$javaVersion $(sed "s#\([^ ]\+\)#-t $REPOSITORY:\1-$VERSION#g" <<< ${tags[@]}) $BASEDIR
   if [ $RELEASE == true ]; then
     docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
     for tag in "${tags[@]}"; do
@@ -23,9 +27,4 @@ do_build() {
   fi
 }
 
-if [ $# == 0 ]; then
-  do_build 8
-  do_build 12
-else
-  do_build $1
-fi
+buildMavenImage 12
