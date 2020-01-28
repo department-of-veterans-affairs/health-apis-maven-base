a727929b32492d2fed2a0eda0b379a922f558553
Bryan Schofield <bryan.schofield@libertyits.com>
2020-01-28 14:44:23 -0500

    VA root is now downloaded and added to the system trusted roots and the JDK trust store.

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
-BASEDIR=$(dirname $0)
+BASEDIR=$(readlink -f $(dirname $0))
 RELEASE=${RELEASE:-false}
 REPOSITORY=vasdvp/health-apis-maven
 VERSION=${VERSION:-$(cat $BASEDIR/VERSION)}
+CERTS=$BASEDIR/certs
 
 #
 # This builds a version of the base image. In the past we supported multiple version, e.g. Java 8 and Java 12.
@@ -16,7 +17,7 @@ buildMavenImage() {
     12) local tags=(3.6-jdk-12) ;;
     *) echo "Unknown Java version: $javaVersion. Supported versions: 12" && exit 1 ;;
   esac
-  docker build -f $BASEDIR/Dockerfile$javaVersion $(sed "s#\([^ ]\+\)#-t $REPOSITORY:\1-$VERSION#g" <<< ${tags[@]}) $BASEDIR
+  docker build --pull -f $BASEDIR/Dockerfile$javaVersion $(sed "s#\([^ ]\+\)#-t $REPOSITORY:\1-$VERSION#g" <<< ${tags[@]}) $BASEDIR
   if [ $RELEASE == true ]; then
     docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
     for tag in "${tags[@]}"; do
@@ -27,4 +28,16 @@ buildMavenImage() {
   fi
 }
 
+
+pullVaCerts() {
+  if [ -d $CERTS ]; then rm -rf $CERTS; fi
+  mkdir $CERTS
+  local vaCertRepo=http://aia.pki.va.gov/PKI/AIA/VA/
+  local vaRootCert=VA-Internal-S2-RCA1-v1.cer
+  echo "Downloading $vaCertRepo/$vaRootCert"
+  local status=$(curl -sw %{http_code} $vaCertRepo/$vaRootCert -o $CERTS/$vaRootCert)
+  if [ "$status" != 200 ]; then echo "Failed to download VA certificates"; exit 1; fi
+}
+
+pullVaCerts
 buildMavenImage 12
